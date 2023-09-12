372b5e51112d1024a66e64d183568cad151d7c06
muhammdfdil.com <muhammadfadilpkl67@gmail.com>
2023-09-12 16:48:12 +0700

    menambahkan file hello.py

---
+++ hello.py
@@ -0,0 +1,22 @@
+a="Hello"
+b="semua"
+c=12
+d=1.5
+print(a+" "+b+","+"apa kabar hari ini?")
+print(type(a))
+print(type(c))
+print(type(d))
+print(1+10**2%2)
+#ini soal kuis
+a=10
+b=2
+c=3
+d=8
+hasil_1 = a / b -c
+hasil_2 = d ** b
+hasil_3 = hasil_1 + hasil_2
+
+hasil_akhir = hasil_3 / b
+
+print("ha silnya =", hasil_akhir)
+         
